0dd9cbc4a947de5e5bc6ad3a6010743a6610ffc0
norman-anderson <thenormanhimself@gmail.com>
2023-04-08 19:50:13 -0400

    Manual Strategy stats and charts

--- ManualStrategy.py
+++ ManualStrategy.py
@@ -37,9 +37,107 @@ class ManualStrategy:
                 action = -1000 - current_position
             else:
                 action = -current_position
+            current_position += action
             portfolio.loc[date, 'JPM'] = action
-            print(action)
         return portfolio
-if __name__ == "__main__":
+
+    def benchmark(self, symbol="JPM",
+                        sd=dt.datetime(2008, 1, 1),
+                        ed=dt.datetime(2009, 12, 31), sv=100000):
+        df_trades = self.testPolicy('JPM', sd, ed, sv)
+        ms_portval = compute_portvals(df_trades, start_val=sv, impact=0, commission=0)
+        ms_normed = ms_portval / ms_portval['Cash'][0]
+
+        df_benchmark = df_trades.copy()
+        df_benchmark.iloc[:] = 0
+        df_benchmark.iloc[0, 0] = 1000
+
+        benchmark_portval = compute_portvals(orders=df_benchmark, start_val=sv, commission=9.95, impact=0.005)
+        #benchmark_normed = benchmark_portval / benchmark_portval['Cash'][0]
+
+        return benchmark_portval
+
+
+def stats(manual, benchmark):
+    manual = manual['Cash']
+    benchmark = benchmark['Cash']
+    manual_cr = manual[-1] / manual[0] - 1
+    manual_daily_return = (manual / manual.shift(1) - 1).iloc[1:]
+    manual_stdev = manual_daily_return.std()
+    manual_mean = manual_daily_return.mean()
+
+    # Benchmark Stats
+    benchmark_cr = benchmark[-1] / benchmark[0] - 1
+    benchmark_daily_return = (benchmark / benchmark.shift(1) - 1).iloc[1:]
+    benchmark_mean = benchmark_daily_return.mean()
+    benchmark_stdev = benchmark_daily_return.std()
+
+    print("Theoretically Optimal Strategy")
+    print("Cumulative return: {:6f}".format(manual_cr))
+    print("Mean of daily returns: {:6f}".format(manual_mean))
+    print("Stdev of daily returns: {:6f}".format(manual_stdev))
+    print("vs.")
+    print("Benchmark")
+    print("Cumulative return: {:6f}".format(benchmark_cr))
+    print("Mean of daily returns: {:6f}".format(benchmark_mean))
+    print("Stdev of daily returns: {:6f}".format(benchmark_stdev))
+
+def chart(trades, manual, benchmark):
+    long = []
+    short = []
+    current = 0
+    last_action = 'OUT'
+    for date in trades.index:
+        current += trades.loc[date, 'JPM']
+        if current < 0:
+            if last_action == 'OUT' or last_action == 'LONG':
+                last_action = 'SHORT'
+                short.append(date)
+        elif current > 0:
+            if last_action == 'OUT' or last_action == 'SHORT':
+                last_action = 'LONG'
+                long.append(date)
+        else:
+            last_action = 'OUT'
+
+        # normalize
+    benchmark['Cash'] = benchmark['Cash'] / benchmark['Cash'][0]
+    manual['Cash'] = manual['Cash'] / manual['Cash'][0]
+
+    plt.figure(figsize=(14,8))
+    plt.title("ManualStragety")
+    plt.xlabel("Date")
+    plt.ylabel("Cumulative Return")
+    plt.xticks(rotation=30)
+    plt.grid()
+    plt.plot(benchmark, label="benchmark", color = "green")
+    plt.plot(manual, label="manual", color = "red")
+
+    for date in short:
+            plt.axvline(date, color = "black")
+    for date in long:
+            plt.axvline(date, color = "blue")
+
+    plt.legend()
+    #plt.savefig("manual.png")
+    plt.show()
+    plt.clf()
+
+def report():
+    # In Sample
     ms = ManualStrategy()
-    ms.testPolicy()
+    trades = ms.testPolicy()
+
+    manual_portvals = compute_portvals(trades)
+    benchmark = ms.benchmark()
+    stats(manual_portvals, benchmark)
+    chart(trades, manual_portvals, benchmark)
+
+
+
+    # Out of Sample
+
+
+if __name__ == "__main__":
+    report()
+
